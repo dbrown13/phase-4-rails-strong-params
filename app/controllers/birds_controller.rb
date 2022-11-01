6bd54f617a61d7ba582e0bd3cfdcc49015a8e28f
Deryn Brown <Deryn.2017@gmail.com>
2022-11-01 11:46:07 -0500

    commit tree 23f29e0acda69478046a051d3842b91ff80fb7b8

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,20 +6,31 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+    # GET /birds/:id
+    def show
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
+
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
   end
+
+
 
 end
